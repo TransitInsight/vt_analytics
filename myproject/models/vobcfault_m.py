6aa64d2a08155e31f4cb59e62d7f3ac78c7c2a33
yueming-zhang <ming.y.zhang@gmail.com>
2020-04-25 13:17:57 -0700

    use response/request json to read from elasticsearch

--- myproject/models/vobcfault_m.py
+++ myproject/models/vobcfault_m.py
@@ -1,60 +1,60 @@
 #%%
 import myproject.config as cfg
 import pandas as pd
-from es.elastic.api import connect
 from datetime import datetime
+import requests
+import json
 
 #%%
 
-def get_count():
-    conn = connect(host=cfg.ElasticSearchDS["host"], port=cfg.ElasticSearchDS["port"])
-    curs = conn.cursor()
-    curs.execute("SELECT faultName, vobcid, count(*) from dlr_vobc_fault where loggedAt > '2015-01-01T12:10:30Z' and loggedAt < '2015-02-02T12:10:30Z' group by faultName, vobcid  LIMIT 10000")
-    df = pd.DataFrame(curs, columns =['FaultName', 'VOBCID', 'FaultCount']) 
+def run_query(query):
+    headers = {'Content-Type': 'application/json'}
+
+    query = {
+        "query": query,
+        "fetch_size":2000
+    }
+
+    response = requests.post("http://win2019:9200/_xpack/sql", headers=headers, data=json.dumps(query))
+
+    df = pd.json_normalize(response.json(),'rows')
+    df.columns = [d['name'] for d in response.json()['columns']]
+
+    for c in response.json()['columns']:
+        dtype = None
+        if (c['type'] == 'text'):
+            dtype = pd.StringDtype()
+        elif (c['type'] == 'datetime'):
+            dtype = 'datetime64'
+        else:
+            dtype = None
+
+        if ( not dtype is None ):
+            df[c['name']] = pd.Series(df[c['name']], dtype=dtype)
+
     return df
 
-def get_faultname():
-    conn = connect(host=cfg.ElasticSearchDS["host"], port=cfg.ElasticSearchDS["port"])
-    curs = conn.cursor()
-    curs.execute("SELECT faultName, count(*) faultName from dlr_vobc_fault group by faultName")
-    df = pd.DataFrame(curs, columns =['FaultName', 'FaultCount']) 
-    return df
-
-
-def get_count_by_fc(fault_name, start_date, end_date):
+def get_count_by(fault_name, start_date, end_date):
     fault_condition = ''
     if (not fault_name.startswith('00') ):
         fault_condition = " and faultName = '{}'".format(fault_name)
-#2014-01-01T00:00:00
+
     if (type(start_date) is datetime):
         start_date = start_date.strftime("%Y-%m-%dT%H:%M:%S")
     if (type(end_date) is datetime):
         end_date = end_date.strftime("%Y-%m-%dT%H:%M:%S")
 
-    conn = connect(host=cfg.ElasticSearchDS["host"], port=cfg.ElasticSearchDS["port"])
-    curs = conn.cursor()
-
-    query = ("SELECT faultName, vobcid, count(*)"
+    query = ("SELECT faultName, vobcid as VOBCID, count(*) as FaultCount"
              " from dlr_vobc_fault "
-             " where loggedAt >= '{}' and loggedAt < '{}' {} "
+             " where vobcid <= 300 and loggedAt >= '{}' and loggedAt < '{}' {} "
              " group by faultName, vobcid "
              " LIMIT 10000 ").format( start_date, end_date, fault_condition)
 
-    curs.execute(query)
-    df = pd.DataFrame(curs, columns =['FaultName', 'VOBCID', 'FaultCount']) 
+    
+    df = run_query(query)
     return df
 
-#%%
-
-
-
-#%%
-
-
-#df = get_count_by_vobc_fc()
-# df = get_count_by_fc('03. FAR Level 3 Fault')
-# df = get_faultname()
-# print (df.describe())
-
-
-# %%
+def get_all_fault():
+    query = "SELECT faultName, count(*) as FaultCount from dlr_vobc_fault group by faultName LIMIT 50"
+    df = run_query(query)
+    return df
