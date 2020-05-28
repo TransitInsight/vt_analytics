38dc94149e40e46499617dfd4be168dd280e9472
Alex <alex.si.zhang@gmail.com>
2020-05-28 14:27:26 -0700

    added table

--- views/ViewFaultListClass.py
+++ views/ViewFaultListClass.py
@@ -32,7 +32,10 @@ class ViewFaultListClass:
 
     def __read_base_data(self):
         df = vobcfault_m.get_fault_list(self.start_date, self.end_date, self.vobc_id, self.fault_code)
-        self.fc_df = df[df['faultCodeSet'] == 1]
+        if len(df.index) != 0:
+            self.fc_df = df[df['faultCodeSet'] == 1]
+        else:
+            self.fc_df = df
 
     def create_fig(self):
         self.create_vobc_fault_list()
@@ -40,8 +43,7 @@ class ViewFaultListClass:
     # add VOBC Fault, shows start time and rectified time
     # regardless actively selected the fault, we already include all fault types
     def create_vobc_fault_list(self):
-
-        if (self.fc_df.empty):
+        if len(self.fc_df.index) == 0:
             return
         # display_fc = self.fc_df[['loggedAt', 'vobcid', 'faultName', 'velocity', 'locationName', 'activePassiveStatus']].copy()
 
--- views/view_scatter.py
+++ views/view_scatter.py
@@ -13,10 +13,12 @@ import plotly.graph_objs as go
 import dash as dash
 import json
 from datetime import datetime as dt
-from datetime import datetime as dt
+from datetime import timedelta
 import re
 
 from modules import module_vobcfault
+from views.ViewTrainmoveClass import ViewTrainmoveClass
+from views.ViewFaultListClass import ViewFaultListClass
 import util as util
 
 
@@ -119,7 +121,10 @@ def _display_click_data(clickData, start_date, end_date, faultcode_):
                 hovermode="closest")
             }
 
-
+def create_fig_fault_list(table_id, fault_code, start_date, end_date, vobc_id):
+    c = ViewFaultListClass(table_id, fault_code, start_date, end_date, vobc_id)
+    c.create_fig()
+    return c.get_fig()
 
 app.layout = html.Div([
 
@@ -151,10 +156,21 @@ layout = html.Div([
             dcc.Graph(id = 'BarGraph', 
                 style={ 'float': 'left', "display":"block", "height" : "33vh",'width': "38vw"},  
             ),
+            # dcc.Graph(id = 'fig_list_dates',
+            #     style={ 'float': 'left', "display":"block", "height" : "33vh",'width': "38vw"}
+            # ),
+            # dash_table.DataTable(id='fig_list_dates',
+                 
+            # )
+            html.Div([create_fig_fault_list('fig_list_dates', -1, filter_start_date, filter_end_date, -1)],
+            style={ 'float': 'left', "display":"block", "height" : "33vh",'width': "38vw"}
+        )
+             
+      ])
         
 
     ])
-])
+
 
 
 @app.callback(
@@ -175,7 +191,82 @@ def update_Scatter(faultcode_,start_date,end_date):
     
     return _update_Scatter(faultcode_,start_date,end_date)
     
+@app.callback(
+    Output('fig_list_dates', 'data'),
+    [
+        Input('fault_code', 'value'),
+        Input('date-range', 'start_date'),
+        Input('date-range', 'end_date') ,
+        Input('Scatterplot', 'clickData'),
+        Input('BarGraph', 'clickData')
+        
+    ])
+def display_figure_fault_list_callback(faultcode_, start_date, end_date, fault_click_value, trend_click_value):
+    start_date,end_date = datecheck(start_date, end_date)
+    faultcode_ = checkfaultcode(faultcode_)
+    return display_figure_fault_list(faultcode_, start_date, end_date, fault_click_value, trend_click_value)
 
+def display_figure_fault_list(value, start_date, end_date, fault_click_value, trend_click_value):    
+    fault_code = value
+    click_fault_code = -1
+    click_vobcid = -1
+    if (fault_click_value != None):
+        click_vobcid = fault_click_value['points'][0]['y']
+        click_fault_code = fault_click_value['points'][0]['curveNumber'] + 1 #click curveNumber is between 0 and 14
+        if (click_fault_code > 15) :
+            click_fault_code -= 15
+        if (fault_code == -1): #if not -1, the dropdown only selected one Fault, so the click must be on the same fault, no need to change
+            fault_code = click_fault_code
+
+    op_date = None
+    if trend_click_value != None:
+        op_date = trend_click_value['points'][0]['x']
+        start_date = util.str2date1(op_date)
+        end_date = start_date + timedelta(days = 1)
+
+    c = ViewFaultListClass('fig_list_dates', -1, start_date, end_date, click_vobcid)
+    d = c.get_data()
+
+    return d
+
+@app.callback(
+    Output('fig_by_trainmove', 'figure'),
+    #Output('clickoutput_bar', 'children'),
+    [
+        Input('fig_by_fault', 'clickData'),
+        Input('fig_by_trend', 'clickData'),
+        Input('fig_fault_list', 'active_cell'),
+        Input('fig_fault_list', 'derived_viewport_data'),
+        Input('vt_session_store', 'data')
+    ]
+    )
+def display_figure_trainmove_callback(first_value, second_value, table_active_cell, table_data, timewindow_value):
+    return display_figure_trainmove(first_value, second_value, table_active_cell, table_data, timewindow_value)
+
+def display_figure_trainmove(first_value, second_value, table_active_cell, table_data, timewindow_value):
+    vobc_id = None
+    fault_code = None
+    if first_value != None:
+        vobc_id = first_value['points'][0]['x']
+        fault_code = first_value['points'][0]['curveNumber'] + 1 #click curveNumber is between 0 and 14
+        if (fault_code > 15) :
+            fault_code -= 15
+
+    op_date = None
+    if second_value != None:
+        op_date = second_value['points'][0]['x']
+
+    offset = 0
+    if timewindow_value != None:
+        offset = timewindow_value['offset']
+
+    delta = timedelta(hours=offset/2)
+
+    if table_data is not None and len(table_data) != 0 and table_active_cell is not None and len(table_data) > table_active_cell['row']:
+        op_date = table_data[table_active_cell['row']]['loggedAt']
+
+    f = create_fig_by_trainmove(vobc_id, op_date, fault_code, delta)
+    return f
 
 if __name__ == "__main__":
     app.run_server()
